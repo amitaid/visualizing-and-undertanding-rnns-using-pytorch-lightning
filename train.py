2b87ddd5351811e29f8c2324db1d9c6dc960d1a8
amitaid <amitai.d@gmail.com>
2021-03-10 17:28:58 +0200

    more training

--- train.py
+++ train.py
@@ -7,9 +7,9 @@ from net.lightning import Lightning
 
 (trl, tel, val), vocab = dataloader.load(FILE_PATH, DEVICE, SPLITS, BATCH_SIZE, SEQ_LEN, unique=True)
 
-for MODEL_NAME in ["lstm"]:  #["rnn", "gru", "lstm"]:
-    for N_LAYERS in [2]:  #[1, 2, 3, 4]:
-        for HIDDEN_SIZE in [32]:  #, 64, 128, 256, 512, 1024]:
+for MODEL_NAME in ["rnn", "gru", "lstm"]: #["lstm"]:  
+    for N_LAYERS in [1, 2, 3]:  #[1, 2, 3, 4]:
+        for HIDDEN_SIZE in [32, 64, 128, 256, 512]:
             net = CharRNN(len(vocab), HIDDEN_SIZE, EMBEDDING_DIM, MODEL_NAME, DROPOUT, N_LAYERS, DEVICE)
             lightning = Lightning(net, LR)
             esc = pl.callbacks.EarlyStopping(monitor='val_acc', min_delta=0.00, patience=3, mode="max")
