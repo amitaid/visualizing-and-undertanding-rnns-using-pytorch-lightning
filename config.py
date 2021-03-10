4579d20e70334abbae77e8fe15663c073a207053
Your Name <you@example.com>
2021-03-10 20:42:50 +0200

    config to output to google drive

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import torch
 _FILE_NAMES = ["warandpeace"] #, "shakespeare"]
 FILE_NAME = _FILE_NAMES[0]
 FILE_PATH = f"data/{FILE_NAME}.txt"
-MODEL_PATH = "models/"
+MODEL_PATH = "/content/gdrive/MyDrive/DL-project/" #"models/"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 N_LAYERS = 2
 SEQ_LEN = 70
